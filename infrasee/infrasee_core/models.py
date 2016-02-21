ee658afe8a877f47b7cbbee9cfe69b273602f946
Curt Spann <curt.spann@gmail.com>
2016-02-20 21:21:15 -0800

    Port model.

--- infrasee/infrasee_core/models.py
+++ infrasee/infrasee_core/models.py
@@ -2,4 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Port(models.Model):
+    name = models.CharField(max_length=256)
+    number = models.IntegerField(max_length=65535)
+    protocol = models.CharField(max_length=3)
+
+    @classmethod
+    def create(cls, name, number, protocol):
+        port = cls(name=name, number=number, protocol=protocol)
+        # do something with the port
+        return port
+
+# example create
+port = Port.create("SSH", 22, "TCP")
