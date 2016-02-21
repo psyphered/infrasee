9fb74c02f1798d7bbd92ac8127439dd52089717a
Curt Spann <curt.spann@gmail.com>
2016-02-21 13:31:44 -0800

    Protocol stored in DB is either TCP or UDP, not 0 or 1. Added view for individual rule.

--- infrasee/infrasee_core/urls.py
+++ infrasee/infrasee_core/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^version$', views.VersionView.as_view(), name='version'),
+    url(r'^port$', views.PortView.as_view(), name='port'),
+    url(r'^port/(?P<pk>[0-9]+)/$', views.PortDetailView.as_view(), name='port_details'),
 ]
--- infrasee/infrasee_core/views.py
+++ infrasee/infrasee_core/views.py
@@ -1,13 +1,41 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.generic import View
+
+from .models import Port
 
 
 class IndexView(View):
+    message = "Index view of infrasee_core"
+
     def get(self, request):
-        return HttpResponse("Index view of infrasee_core")
+        # <view logic>
+        return HttpResponse(self.message)
 
 
 class VersionView(View):
-    def get(self, request):
-        return HttpResponse('{"version":"1.0.0-DEV"}')
+    version = '{"version":"1.0.0-DEV"}'
 
+    def get(self, request):
+        # <view logic>
+        return HttpResponse(self.version)
+
+
+class PortView(View):
+    model = Port
+
+    def get(self, request):
+        all_ports_list = Port.objects.all()
+        return HttpResponse(all_ports_list)
+
+
+class PortDetailView(View):
+    model = Port
+
+    def get(self, request, pk):
+        port_details = Port.objects.get(id=pk)
+
+        return HttpResponse(port_details)
+
+    def post(self, request, pk):
+        did_some_stuff = "stuff done"
+        return HttpResponse(did_some_stuff)
