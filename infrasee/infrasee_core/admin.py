23ce810a9bd45a301b69d4eb2a19a3135ae4abdc
pstackle <pstackle@gmail.com>
2016-02-21 06:55:54 +0000

    add models: application, environment, host, data center

--- infrasee/infrasee_core/admin.py
+++ infrasee/infrasee_core/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
 from .models import Port
+from .models import Application
+from .models import Environment
+from .models import Host
+from .models import DataCenter
 
 admin.site.register(Port)
+admin.site.register(Application)
+admin.site.register(Environment)
+admin.site.register(Host)
+admin.site.register(DataCenter)
